43decf282b8cf361b7980dfa74674a11f3873f7d
phemankumar <heman_kumar76@yahoo.com>
2025-03-07 21:40:47 -0500

    Add files via upload
    Day1 programs

---
+++ day1/Hellotestleaf.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class Hellotestleaf {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Testleaf learning");
+
+		
+	}
+
+
+}
